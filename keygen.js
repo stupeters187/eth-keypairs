3e8cae73566ffd5c4e5263f4b8c3efd727897235
stupeters187 <stu_peters187@hotmail.com>
2017-02-14 09:04:44 -0500

    Update keygen.js

--- keygen.js
+++ keygen.js
@@ -1,5 +1,4 @@
 var EthUtil = require("ethereumjs-util")
-// console.log(`0:${process.argv[0]}\n1:${process.argv[1]}\n2:${process.argv[2]}\n`)
 
 var hexToBytes = function(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
